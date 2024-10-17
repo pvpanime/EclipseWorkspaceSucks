7122672c48857bb343a12cd14191daaff4e237b1
kimdh32022 <kimdh32022@naver.com>
2024-10-17 13:56:49 +0900

    인사말

---
+++ JavaParctice3/src/ex_forkTest/Dohyeon0222.java
@@ -0,0 +1,10 @@
+package ex_forkTest;
+
+public class Dohyeon0222 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("종호형님 안녕하세요?");
+	}
+
+}
